47403705dc9927b30b7acca3482bbe99984b36e1
Vineesha93 <70128124+Vineesha93@users.noreply.github.com>
2021-11-23 18:55:43 +0530

    Update data.py
    deleted sd() and made changes in code

--- data.py
+++ data.py
@@ -7,12 +7,14 @@ import plotly.graph_objects as go
 
 df=pd.read_csv("code7.csv")
 data=df["population_mean"].tolist()
-
+fig = ff.create_distplot([data], ["reading_time"], show_hist=False)
+fig.show()
+print("Mean ",statistics.mean(data))
 
 def random_setofmean(counter):
     dataset=[]
     for i in range(0,counter):
-        random_index=random.randint(0,len(data)-1)
+        random_index=random.randint(0,len(data))
         value=data[random_index]
         dataset.append(value)
     mean=statistics.mean(dataset)
@@ -20,10 +22,9 @@ def random_setofmean(counter):
 
 def show_fig(mean_list):
     df=mean_list
-    mean=statistics.mean(df)
-    print("mean of sampling distribution: ",mean)
-    fig=ff.create_distplot([df],["population_mean"],show_hist=False)
-    fig.add_trace(go.Scatter(x=[mean,mean],y=[0,1],mode="lines",name="mean"))
+    
+    fig=ff.create_distplot([df],["reading_mean"],show_hist=False)
+   
     fig.show()
 
 def setup():
@@ -32,14 +33,15 @@ def setup():
         set_of_means=random_setofmean(100)
         mean_list.append(set_of_means)
     show_fig(mean_list)
+     print("Sample ", statistics.mean(mean_list))
 setup()
 
-def sd():
-    mean_list=[]
-    for i in range(0,1000):
-        set_of_means=random_setofmean(100)
-        mean_list.append(set_of_means)
+# def sd():
+#     mean_list=[]
+#     for i in range(0,1000):
+#         set_of_means=random_setofmean(100)
+#         mean_list.append(set_of_means)
     
-    sd=statistics.stdev(mean_list)
-    print("sd",sd)
+#     sd=statistics.stdev(mean_list)
+#     print("sd",sd)
 sd()
